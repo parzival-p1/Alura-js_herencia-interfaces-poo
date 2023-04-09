a235d8b8a540e3ea3508c5c01c712b72ef2a773d
Paco Hdez <francj.hdez@gmail.com>
2023-04-09 13:30:29 -0600

    adding cuenta.js

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 //* *** Importación de clases *** *//
 import { Cliente } from "./cliente.js";
-import { cuentaCorriente } from "./ctaCorriente.js";
-import { cuentaAhorro} from "./ctaAhorro.js";
+//import { cuentaCorriente } from "./ctaCorriente.js";
+//import { cuentaAhorro} from "./ctaAhorro.js";
+import { Cuenta } from "./cuenta.js";
 
 //* CTES
 const cliente = new Cliente('Paco', '12345678', 'HETFJ0303937FJHT');
 const cliente2 = new Cliente('Maria', '87654321', 'TEMIJ0404947MJHT');
 
 //* CTAS
-const cuentaDePaco = new cuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new cuentaCorriente(cliente2, '2', '002');
+const cuentaDePaco = new Cuenta(cliente, '1', '001', 0);
+const cuentaDeMaria = new Cuenta(cliente2, '2', '002', 0);
 
 //* CTAS AHORRO
-const cuentaAhorroPaco = new cuentaAhorro(cliente, '12345678', '001', 0);
+const cuentaAhorroPaco = new Cuenta(cliente, '12345678', '001', 0);
+
 console.log(cuentaDePaco);
+cuentaDePaco.depositoEnCta(150);
+console.log(cuentaDePaco.verSaldo());
+cuentaDePaco.retirarEnCta(70);
+console.log(cuentaDePaco.verSaldo());
 console.log(cuentaAhorroPaco);
+cuentaAhorroPaco.depositoEnCta(200);
+console.log(cuentaAhorroPaco.verSaldo());
+cuentaAhorroPaco.retirarEnCta(120);
+console.log(cuentaAhorroPaco.verSaldo());
+
